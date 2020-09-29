470733bd502f4d5fe2096213fd228e75651a42e1
Jack Summers <jacksummers715@gmail.com>
2020-09-29 07:51:30 -0400

    Revert "test of some permissions hackery"
    This reverts commit 320eeb575d5641016d01289bdbf329f6ae9fc243.
    
    Change-Id: I60996b7b97e940b0ed109a6bdcbe7b51bad4e0e1

--- common.mk
+++ common.mk
@@ -530,12 +530,4 @@ PRODUCT_BOOT_JARS += \
     
 TARGET_HAS_FOD := true
 
-# Permission Hackery 
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.control_privapp_permissions=disable \
-    ro.adb.secure=0 \
-    persist.sys.usb.config=mtp,adb \
-    persist.service.debuggable=1 \
-    persist.service.adb.enable=1
-
 BUILD_RR_WALLPAPERS:= true
